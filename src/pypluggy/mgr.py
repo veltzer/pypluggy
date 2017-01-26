fcdad5121e9c9c1751c7c58033d4de200e7c21df
Mark Veltzer <mark.veltzer@gmail.com>
2017-01-26 14:21:48 +0200

    pass type

--- src/pypluggy/mgr.py
+++ src/pypluggy/mgr.py
@@ -65,13 +65,14 @@ class Mgr:
         self.classes_instantiated[cls] = inst_set
         return inst_set
 
-    def list_names(self, cls=None):
+    def list_names(self, cls=None, check_type=None):
         assert cls is not None
+        assert check_type is not None
         results = []
         for module in self.modules_loaded:
             for name, t in module.__dict__.items():
                 logger.debug("trying <%s> <%s> <%s>", name, t.__class__.__name__, cls)
-                if type(t) is type and issubclass(t, cls):
+                if type(t) is check_type and issubclass(t, cls):
                     logger.debug("appending <%s>", name)
                     results.append(t.__name__)
         return results
