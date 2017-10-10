88e660e8a659dfc2f6c7900e4eda4fbd3b82770a
Mark Veltzer <mark.veltzer@gmail.com>
2017-10-10 14:39:41 +0300

    setup.py filled

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 import setuptools
 
-import sys
-if not sys.version_info[0] == 3:
-    sys.exit("Sorry, only python version 3 is supported")
-
 setuptools.setup(
     name='pypluggy',
     version='0.0.10',
     description='pypluggy is a lightweight plugin framework for python',
     long_description='pypluggy is a lightweight plugin framework for python',
-    url='https://veltzer.github.io/pypluggy',
+    url='https://github.com/veltzer/pypluggy',
+    download_url='https://github.com/veltzer/pypluggy',
     author='Mark Veltzer',
     author_email='mark.veltzer@gmail.com',
+    maintainer='Mark Veltzer',
+    maintainer_email='mark.veltzer@gmail.com',
     license='MIT',
+    platforms=['python3'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
