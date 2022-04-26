176eaf05344252c3167f051f15cfa07167b6b05b
Mark Veltzer <mark.veltzer@gmail.com>
2022-04-26 23:45:19 +0300

    missing files

--- config/project.py
+++ config/project.py
@@ -22,7 +22,7 @@ project_keywords = [
     "python",
 ]
 project_license = "MIT"
-project_year_started = "2017"
+project_year_started = 2017
 project_description = project_short_description
 project_platforms = [
     "python3",
