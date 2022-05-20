051a7964f0af1ec83c56c682ea0173cdf12f0cba
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-20 22:13:01 +0300

    fixing the build

--- config/project.py
+++ config/project.py
@@ -1,4 +1,4 @@
-short_description = "pypluggy is a lightweight plugin framework for python"
+description_short = "pypluggy is a lightweight plugin framework for python"
 keywords = [
     "plugin",
     "python",
