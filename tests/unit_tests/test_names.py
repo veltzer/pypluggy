b694d379b5a2959d8ed1e7a18a677d5a36412942
Mark Veltzer <mark.veltzer@gmail.com>
2025-08-28 23:29:28 +0300

    no commit message given

--- tests/unit_tests/test_names.py
+++ tests/unit_tests/test_names.py
@@ -16,11 +16,11 @@ class TestNames(unittest.TestCase):
     def testList(self):
         m = Mgr()
         m.load_modules()
-        self.assertListEqual(m.list_names(cls=unittest.TestCase, check_type=type), ['TestNames'], msg='uh uh')
+        self.assertListEqual(m.list_names(cls=unittest.TestCase, check_type=type), ["TestNames"], msg="uh uh")
 
     @unittest.skip("doesnt work")
     def testInstantiate(self):
         m = Mgr()
         m.load_modules()
-        t = m.instantiate_name(cls=unittest.TestCase, name='TestNames')
+        t = m.instantiate_name(cls=unittest.TestCase, name="TestNames")
         self.assertIsInstance(t, unittest.TestCase)
