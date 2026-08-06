2b15c173681f0ebe5dcc2f8a1115bb4db6017852
Mark Veltzer <mark.veltzer@gmail.com>
2026-08-06 12:09:22 +0300

    pydmt -> rsconstruct

---
+++ config/project.lua
@@ -0,0 +1,6 @@
+NAME = "pypluggy"
+DESCRIPTION_SHORT = "pypluggy is a lightweight plugin framework for python"
+KEYWORDS = {
+    "plugin",
+    "python",
+}
--- config/project.py
+++
@@ -1,7 +0,0 @@
-""" project definitions """
-name = "pypluggy"
-description_short = "pypluggy is a lightweight plugin framework for python"
-keywords = [
-    "plugin",
-    "python",
-]
